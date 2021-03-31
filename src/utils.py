7041fcc2bf009158082575f4c1afd1309a0a9aff
MarieDeSmedt <desmedtmarie@outlook.com>
2021-03-31 09:53:04 +0200

    finish vitesse moyenne

--- src/utils.py
+++ src/utils.py
@@ -90,3 +90,5 @@ def ft_degree(lat1, lng1, lat2, lng2):
     y = np.sin(lng_delta_rad) * np.cos(lat2)
     x = np.cos(lat1) * np.sin(lat2) - np.sin(lat1) * np.cos(lat2) * np.cos(lng_delta_rad)
     return np.degrees(np.arctan2(y, x))
+
+##################################################################################
